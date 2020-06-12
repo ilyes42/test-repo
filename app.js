534f11dee03bf5bc483648e9be6609f02c6840e0
Ilyes Hammemi <ilyeshammemi42@gmail.com>
2020-06-12 19:24:27 +0100

    added /birds route

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const birds = require('./birds')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/birds', birds)
+
+app.get('/', (req, res) => res.send('App home page'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
